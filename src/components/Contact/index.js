23f9a1826d9f3fd844aef4bbc3bfd0a1a131d5ed
Luis <m.luishernandez@outlook.com>
2021-06-02 14:40:31 -0400

    Added icons to contact page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,20 +1,27 @@
+import { FaLinkedin } from 'react-icons/fa';
+import { GrMail } from 'react-icons/gr';
+
+const style = {
+    icons: {
+        fontSize: 64,
+    }
+}
+
 
 const Contact = () => {
     return (
-        <div className="wrapper">
-            <section id="contact">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-8 mx-auto">
-                            <h1>Contact Me</h1>
-                            <hr class="separator separator--line" />
-                            <p class="lead">Email:m.luishernandez@outlook.com</p>
-                            <a href="m.luishernandez@outlook.com"></a>
-                        </div>
-                    </div>
+
+        <section id="contact">
+            <div class="row">
+                <div class="col-lg-8 mx-auto">
+                    <h1>Contact Me</h1>
+                    <hr class="separator separator--line" />
+                    <a href="mailto:m.luishernandez@outlook.com" style={style.icons}><GrMail /></a>
+                    <a href="https://www.linkedin.com/in/luis-hernandez-1a55811b4/" target="_blank" style={style.icons}><FaLinkedin /></a>
+
                 </div>
-            </section>
-        </div>
+            </div>
+        </section>
     );
 }
 
