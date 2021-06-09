3b212e55bc029c29b6c25374154b7505e57f0245
Luis <m.luishernandez@outlook.com>
2021-06-09 16:34:41 -0400

    Took iframe out. Need better styling

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -30,8 +30,8 @@ const About = () => {
                     <p class="lead">
                         I'm a web developer with 3 years of experience in electronics manufacturing and management. Goal oriented and passionate about learning new technologies. Proficient in english and spanish. Currently living in Kissimmee, Florida. I finished an associates in networking and got a certificate as a Full Stack Web Dev from the UCF.
                     </p>
-                    <iframe src="https://api.badgr.io/public/assertions/-aTK6j42SuyJlqlTgy9QBQ?embedVersion=1&amp;embedWidth=330&amp;embedHeight=186&amp;identity__email=m.luishernandez%40outlook.com" title="Badge: UCF Coding Boot Camp (Full-Stack Web Development)[Full-Time]" >
-                    </iframe>
+                    {/* <iframe src="https://api.badgr.io/public/assertions/-aTK6j42SuyJlqlTgy9QBQ?embedVersion=1&amp;embedWidth=330&amp;embedHeight=186&amp;identity__email=m.luishernandez%40outlook.com" title="Badge: UCF Coding Boot Camp (Full-Stack Web Development)[Full-Time]" >
+                    </iframe> */}
                 </div>
 
                 <div class="col-lg-2 mx-auto">
